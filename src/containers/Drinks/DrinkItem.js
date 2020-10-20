61a8d5930ede373f017dfb1ded4674f0b9e401a0
Zack <altschulerzack@yahoo.com>
2020-10-19 18:54:54 -0700

    first commit

--- src/containers/Drinks/DrinkItem.js
+++ src/containers/Drinks/DrinkItem.js
@@ -16,7 +16,7 @@ export default function DrinkItem(props) {
           className="DrinkItem_Delete"
           onClick={() => props.onClickDelete(props.id)}
         >
-          Delete
+          deletus
         </button>
       </div>
     </li>
